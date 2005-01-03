3bccdb34b4282bff0dd5888140659216dac8943d
Saleem Abdulrasool <compnerd@gentoo.org>
2005-01-03 06:36:17 +0000

    JavaExceptions.py : Initial Import
    svn path=/gentoo-java-experimental/java-config-ng/trunk/; revision=7

---
+++ JavaExceptions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Copyright 2004 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Header: $
+
+# Author: Saleem Abdulrasool <compnerd@compnerd.org>
+# Maintainer: Gentoo Java Herd <java@gentoo.org>
+# Java Subsystem Configuration Utility for Gentoo Linux
+
+# ChangeLog
+# Saleem A. <compnerd@compnerd.org>
+#     December 30, 2004 - Initial Rewrite
+#                       - Based on the collective works of the following:
+#                         {karltk,axxo,aether}@gentoo.org
+
+class JavaHomeUndefinedError(SystemExit):
+   """
+   $JAVA_HOME is undefined!
+   """
+
+class EnvironmentUnexecutableError(SystemExit):
+   """
+   The file is not executable!
+   """
+
+class JavaConfig_RuntimeError(SystemExit):
+   """
+   General Exception for the java-config utility
+   """
