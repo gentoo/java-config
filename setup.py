64ad2b71c521c9ac6d4ecdef8b7065515697f62b
Thomas Matthijs <axxo@gentoo.org>
2005-11-10 13:39:32 +0000

    fix manual loc
    svn path=/java-config-ng/branches/axxo/; revision=1265

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setup (
 	name = 'java-config',
-	version = '2.0.15',
+	version = '2.0.16',
 	description = 'java enviroment configuration tool',
 	long_description = \
 	"""
@@ -20,7 +20,7 @@ setup (
 	package_dir = { 'java_config' : 'src/java_config' },
 	scripts = ['src/java-config','src/depend-java-query','src/run-java-tool', 'src/gjl'],
 	data_files = [
-		('man/man1', ['man/java-config.1']),
+		('share/man/man1', ['man/java-config.1']),
 		('share/java-config/launcher', ['src/launcher.bash']),
 		('share/eselect/modules', ['src/eselect/java.eselect']),
 		('/etc/java-config/', ['config/jdk.conf','config/symlink-tools','config/compilers.conf','config/virtuals']),
