e16732dc412a2b6cc427b08bd10f145e97062434
Joshua Nichols <nichoj@gentoo.org>
2006-03-24 13:56:51 +0000

    Some fixes regarding the version number.
    svn path=/java-config-ng/branches/axxo/; revision=2024

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from os import listdir
 
 setup (
 	name = 'java-config',
-	version = '2.0.20',
+	version = '2.0.21',
 	description = 'java enviroment configuration tool',
 	long_description = \
 	"""
--- src/java_config/__init__.py
+++ src/java_config/__init__.py
@@ -2,4 +2,4 @@
 java-config support files
 '''
 
-__version__ = "2.0.0"
+__version__ = "2.0.21"
