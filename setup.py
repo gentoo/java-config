9d6a7f2187d5b2531813a01b5e739e7a916a1cf7
Joshua Nichols <nichoj@gentoo.org>
2006-06-30 12:06:00 +0000

    Added java-nsplugin.eselect
    svn path=/java-config-ng/branches/axxo/; revision=2404

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from os import listdir
 
 setup (
 	name = 'java-config',
-	version = '2.0.22',
+	version = '2.0.26',
 	description = 'java enviroment configuration tool',
 	long_description = \
 	"""
@@ -14,7 +14,7 @@ setup (
 		variables and configuration files involved in the java
 		enviroment for Gentoo Linux.
 	""",
-	maintainer = 'Gentoo Java Herd',
+	maintainer = 'Gentoo Java Team',
 	maintainer_email = 'java@gentoo.org',
 	url = 'http://www.gentoo.org',
 	#packages = ['java_config'],
@@ -24,7 +24,7 @@ setup (
 		('share/java-config-2/pym/java_config/', ['src/java_config/'+file for file in listdir('src/java_config/')] ),
 		('share/man/man1', ['man/java-config-2.1']),
 		('share/java-config-2/launcher', ['src/launcher.bash']),
-		('share/eselect/modules', ['src/eselect/java-vm.eselect']),
+		('share/eselect/modules', ['src/eselect/java-vm.eselect','src/eselect/java-nsplugin.eselect']),
 		('/etc/java-config-2/', ['config/virtuals']),
 		('/etc/java-config-2/build/', ['config/jdk.conf','config/compilers.conf']),
 		('/etc/env.d/',['config/20java-config']),
