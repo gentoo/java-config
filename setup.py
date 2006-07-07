d1c1e955e60919dbff65352ed8bbfda40f6429b2
Joshua Nichols <nichoj@gentoo.org>
2006-07-07 02:25:29 +0000

    Updated setup.py for version
    svn path=/java-config-ng/branches/axxo/; revision=2451

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from os import listdir
 
 setup (
 	name = 'java-config',
-	version = '2.0.26',
+	version = '2.0.27',
 	description = 'java enviroment configuration tool',
 	long_description = \
 	"""
