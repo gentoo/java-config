c178e2938db696a7efd2a2fed0bc383dd88d5f21
Alistair Bush <ali_bush@gentoo.org>
2009-02-24 10:54:42 +0000

    Updated unittests.  Made unittests provide there own packages, vms, virtuals, etc to provide known state to test against.
    svn path=/projects/java-config-2/trunk/; revision=7552

--- src/testsuite/Package.py
+++ src/testsuite/Package.py
@@ -1,8 +1,27 @@
 import unittest
 import os
 
+from java_config_2.Package import Package
+
 class TestPackage(unittest.TestCase):
 	path = os.path.join(os.path.dirname(__file__), 'packages', '%s/package.env')
+
+	def load_package(self, package):
+		config = TestPackage.path % package
+		return Package(package, config)
+
+	def setUp(self):
+		self.ant = self.load_package('ant-cores')
+
+	def test_package_info(self):
+		#using a package we definitely
+		#know will not be into the tree
+		#to ensure we ain't using real system packages.
+		self.assertEqual(self.ant.name(), 'ant-cores')
+		self.assertEqual(self.ant.description(), "Description: %s" % self.ant.name())
+		self.assertEqual(self.ant.target(), "1.4")
+		self.assertTrue(self.ant.query('JAVADOC_PATH'))
+		self.assertFalse(self.ant.query('VAR_SHOULD_NOT_EXIST'))
 
 if __name__ == '__main__':
 	unittest.main()
--- src/testsuite/Virtual.py
+++ src/testsuite/Virtual.py
@@ -1,22 +1,35 @@
 import unittest
+import os
 from java_config_2.Virtual import Virtual
 from java_config_2.EnvironmentManager import EnvironmentManager as em
-import os
+from java_config_2.Errors import ProviderUnavailableError
+
+def load_virtual(virtual):
+	config = os.path.join(TestVirtual.path, virtual)
+	return Virtual(virtual, em, config)
 
 class TestVirtual(unittest.TestCase):
 	path = os.path.join(os.path.dirname(__file__), "virtual_configs") + "/"
 
-	def load_virtual(self, virtual):
-		config = os.path.join(TestVirtual.path,virtual)
-		return Virtual(virtual, em, config)
-
 	def setUp(self):
-		self.jaf = self.load_virtual('jaf')
-		self.jdbc = self.load_virtual('jdbc')
+		self.jaf = load_virtual('jaf')
 
 	def test_get_vms(self):
 		self.assertEqual(self.jaf.get_vms(), ['sun-jdk-1.6'])
+
+class TestMultiProviderVirtual(unittest.TestCase):
+
+	def setUp(self):
+		em.set_active_vm(em.get_vm('sun-jdk-1.6'))
+		self.jdbc = load_virtual('jdbc')
 	
+	def test_classpath_multiple(self):
+		self.assertEqual( len(self.jdbc.classpath().split(':')), 2)
+
+	def test_invalid_vm_error(self):
+		em.set_active_vm(em.get_vm('ibm-jdk-bin-1.5'))
+		self.assertRaises( ProviderUnavailableError, self.jdbc.classpath )
+
 if __name__ == '__main__':
 	unittest.main()
 
