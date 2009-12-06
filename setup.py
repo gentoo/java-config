5ebc36c6501fc4cfa345e2640e616439e5bc71ee
Alistair Bush <ali_bush@gentoo.org>
2009-12-06 00:22:45 +0000

    Updated version.
    svn path=/projects/java-config-2/trunk/; revision=8004

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ from glob import glob
 setup (
 	cmdclass={'install_scripts': my_install_scripts},
 	name = 'java-config',
-	version = '2.1.9',
+	version = '2.1.10',
 	description = 'java enviroment configuration tool',
 	long_description = \
 	"""
