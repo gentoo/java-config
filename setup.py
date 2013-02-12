8cd7f691028da63728831fa537a7048de6eee2a9
Ralph Sennhauser <sera@gentoo.org>
2013-02-12 16:06:27 +0000

    Generate jdk-defaults.conf at install, if nothing else thist least this is a lot more maintainable than two dozen files.
    svn path=/projects/java-config-2/trunk/; revision=9198

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ package_version = '2.2.0'
 #######################################
 
 from distutils.command.build import build
+from distutils.command.install import install
 import fileinput, os, sys
 
 class my_build(build):
@@ -21,10 +22,55 @@ class my_build(build):
 					sys.stdout.write(line.replace('@GENTOO_PORTAGE_EPREFIX@', eprefix))
 
 
+class my_install(install):
+	"""
+	Generate and install the jdk defaults configuration file.
+
+	For the most part useless, wasn't updated in a long time either and doesn't
+	reflect reality anymore. Doing it here is at least a lot more maintainable
+	then a couple dozen files where it was defined before.
+	Also what should be default is a downstream decision and java-config
+	shouldn't have any business here. Still keeping it for the time being.
+	"""
+
+	def run(self):
+		install.run(self)
+
+		arch = os.getenv('ARCH', 'unknown')
+		defaults = '*= icedtea'
+		if arch in ['amd64', 'x86']:
+			defaults = '*= icedtea6 icedtea6-bin sun-jdk blackdown-jdk ibm-jdk-bin jrockit-jdk-bin'
+		elif arch in ['ppc-macos', 'x64-macos', 'x86-macos']:
+			defaults = '*= apple-jdk-bin'
+		elif arch in ['amd64-fbsd', 'x86-fbsd', 'x64-freebsd', 'x86-freebsd']:
+			defaults = '*= diablo-jdk'
+		elif arch in ['sparc-solaris', 'sparc64-solaris', 'x64-solaris', 'x86-solaris']:
+			defaults = '*= sun-jdk'
+		elif arch in ['mips', 'sparc']:
+			defaults = '*= blackdown-jdk'
+		elif arch in ['ppc', 'ppc64', 'ppc-linux', 'ppc-aix']:
+			defaults = '*= ibm-jdk-bin'
+		elif arch in ['alpha']:
+			defaults = '*= compaq-jdk'
+		elif arch in ['arm']:
+			defaults = '*= icedtea-7 icedtea-6 icedtea-bin-7 icedtea-bin-6'
+		elif arch in ['ia64']:
+			defaults = '*= jrockit-jdk-bin'
+		elif arch in ['hppa']:
+			defaults = '*= kaffe'
+		elif arch in ['hpux']:
+			defaults = '*= hp-jdk-bin'
+
+		os.mkdir(self.root + '/usr/share/java-config-2/config/')
+		with open(self.root + '/usr/share/java-config-2/config/jdk-defaults.conf', 'w') as f:
+			f.write("# This files contain the default support jdk's\n")
+			f.write(defaults + "\n")
+
+
 from distutils.core import setup
 
 setup (
-	cmdclass={'build' : my_build},
+	cmdclass={'build' : my_build, 'install' : my_install},
 	name = 'java-config',
 	version = package_version,
 	description = 'java enviroment configuration tool',
